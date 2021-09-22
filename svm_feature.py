64dccf9497b04c520c98d7aa67f058ff4c8588c0
albertyang <albertyang@soundpie.cn>
2021-09-22 09:38:09 +0800

    svm feature

--- svm_feature.py
+++ svm_feature.py
@@ -1,7 +1,7 @@
 from sklearn.manifold import TSNE
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report
 from model_design import AutoEncoder
 from data import get_dataset
 import torch
@@ -18,24 +18,23 @@ if __name__ == '__main__':
     for i, train_data_x in enumerate(train_loader):
         if i > 0:
             break
-    train_x_feature, _ = auto_encoder_model(train_data_x)
-    train_x_feature = train_x_feature.data.numpy()
+    train_x_encoder, _ = auto_encoder_model(train_data_x)
+    train_x_encoder = train_x_encoder.data.numpy()
     train_y = train_data_y.data.numpy()
 
     for i, test_data_x in enumerate(test_loader):
         if i > 0:
             break
-    test_x_feature, _ = auto_encoder_model(test_data_x)
-    test_x_feature = test_x_feature.data.numpy()
+    test_x_encoder, _ = auto_encoder_model(test_data_x)
+    test_x_encoder = test_x_encoder.data.numpy()
     test_y = test_data_y.data.numpy()
 
     pca_model = PCA(n_components=3, random_state=100)
-    train_pca_x = pca_model.fit_transform(train_data_x.data.numpy())
-    test_pca_x = pca_model.fit_transform(test_data_x.data.numpy())
-    print(train_pca_x.shape)
+    pca_train_x = pca_model.fit_transform(train_data_x.data.numpy())
+    pca_test_x = pca_model.fit_transform(test_data_x.data.numpy())
+    print(f"pca_train_x.shape: {pca_train_x.shape}")
     
     encoder_svc = SVC(kernel='rbf', random_state=123)
-    encoder_svc.fit(train_x_feature, train_y)
-    pred_svc = encoder_svc.predict(test_x_feature)
+    encoder_svc.fit(train_x_encoder, train_y)
+    pred_svc = encoder_svc.predict(test_x_encoder)
     print(classification_report(test_y, pred_svc))
-    print(f"Accuracy: {accuracy_score(test_y, pred_svc)}")
